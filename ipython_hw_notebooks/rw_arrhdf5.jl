a7e63d1b17e746696aee515430d6626d401f9ab0
gummiks <gummiks@gmail.com>
2014-02-07 17:58:15 -0500

    More docs

--- ipython_hw_notebooks/rw_arrhdf5.jl
+++ ipython_hw_notebooks/rw_arrhdf5.jl
@@ -1,8 +1,8 @@
-
 using HDF5, JLD
 
 function write_arrhdf5(data::Array, filename::String, varname::String)
-    @assert typeof(data) == Array{Float64,1}
+    #A function that writes the array **data** to a file **filename** in HDF5 file type format; storing it as a variable **varname**.
+
     #We need to check if the file is there, as h5write cannot overwrite
     if(isfile(filename))
         rm(filename)
@@ -11,6 +11,7 @@ function write_arrhdf5(data::Array, filename::String, varname::String)
 end
 
 function read_arrhdf5(filename::String, varname::String)
+    #A function that reads the variable **varname** stored in file **filename** 
     @assert isfile(filename)
     return h5read(filename,varname)
 end
